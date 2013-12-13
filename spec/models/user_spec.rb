b24091cc8ec3c4eccfb3dc31f8f2b0f133c62109
Phan Duy <pduy1206@gmail.com>
2013-12-13 04:15:45 +0000

    logout handle, cucumber generate

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,9 @@ describe User do
   it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -82,5 +85,10 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
